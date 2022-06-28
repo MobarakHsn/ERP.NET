03b46531b9ff1aafa0337fc3570ea8ab1dd9d31e
musanna-nafis <hasanalmusanna7@gmail.com>
2022-06-29 03:35:42 +0600

    FinanceDb

---
+++ ERP.NET/ERP/Controllers/Finance/TestController.cs
@@ -0,0 +1,20 @@
+﻿using ERP.Finance_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERP.Controllers.Finance
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            var db = new ERPEntities1();
+            var ware = db.Warehouses.ToList();
+            return View(ware);
+        }
+    }
+}
